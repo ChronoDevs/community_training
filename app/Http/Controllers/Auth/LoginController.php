c36295d2e652313381454279e5255e2137c3c979
VinceCab <102521477+LupusNocti@users.noreply.github.com>
2023-08-24 17:55:16 -0700

    Can log in using User's name or Admin's name

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,113 +5,48 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Socialite\Facades\Socialite;
-use App\Models\User;
 
 class LoginController extends Controller
 {
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
 
-    /**
-     * Get the post-logout redirect path.
-     *
-     * @return string
-     */
-    public function redirectTo()
+    public function showLoginForm()
     {
-        return RouteServiceProvider::HOME;
+        return view('auth.login'); // Customize this view as needed
     }
 
-    /**
-     * Redirect the user to the Facebook authentication page.
-     *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    public function redirectToFacebook()
+    protected function guard()
     {
-        return Socialite::driver('facebook')->redirect();
+        return Auth::guard('admin'); // Use the 'admin' guard for administrators
+        return Auth::guard('web');
     }
-
-    /**
-     * Obtain the user information from Facebook.
-     *
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function handleFacebookCallback()
+    
+    protected function authenticated(Request $request, $user)
     {
-        $user = Socialite::driver('facebook')->user();
-
-        $this->registerOrLoginUser($user);
-
-        // Returns to home after login
-        return redirect()->route('home');
-    }
-
-    /**
-     * Redirect the user to the Google authentication page.
-     *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    public function redirectToGoogle()
-    {
-        return Socialite::driver('google')->redirect();
-    }
-
-    /**
-     * Obtain the user information from Google.
-     *
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function handleGoogleCallback()
-    {
-        $user = Socialite::driver('google')->user();
-
-        $this->registerOrLoginUser($user);
-
-        // Returns to home after login
-        return redirect()->route('home');
-    }
-
-    /**
-     * Register or login the user based on provided data.
-     *
-     * @param  \Laravel\Socialite\AbstractUser  $data
-     * @return void
-     */
-    protected function registerOrLoginUser($data)
-    {
-        $existingUser = User::where('email', $data->email)->first();
-
-        if ($existingUser) {
-            Auth::login($existingUser);
+        if ($user instanceof \App\Models\Admin) {
+            return redirect()->route('admin.home');
         } else {
-            $user = new User();
-            $user->name = $data->name;
-            $user->email = $data->email;
-            $user->provider_id = $data->id;
-            $user->avatar = $data->avatar;
-            $user->password = bcrypt('random_password'); // Set a temporary random password
-            $user->save();
-
-            Auth::login($user);
+            return redirect()->route('home');
         }
+    }
+    
+    protected function username()
+    {
+        $input = request()->input('email');
+    
+        $fieldType = filter_var($input, FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
+    
+        request()->merge([$fieldType => $input]);
+    
+        return $fieldType;
     }
 }
