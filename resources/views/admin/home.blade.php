626d98037a4a89889f26cee3860c3ae3fad1551c
VinceCab <102521477+LupusNocti@users.noreply.github.com>
2023-08-29 23:07:23 -0700

    added appropriate css and html for the admin sidebar

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,6 +5,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-3">
+            @include('adminsidebar')
         </div>
         <div class="col-md-9">
             <div class="dashboard-header">
