d82dd62476632f374349f1c34f584f726ce061ba
VinceCab <102521477+LupusNocti@users.noreply.github.com>
2023-09-25 23:42:48 -0700

    admin ui optimization

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -6,6 +6,9 @@ use App\Http\Requests\Admin\CreateCategoryRequest;
 use App\Http\Requests\Admin\CreateTagRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
+use App\Models\Listing;
+use App\Models\Favorite;
+use App\Models\ListingLike;
 
 class DashboardController extends Controller
 {
@@ -16,6 +19,11 @@ class DashboardController extends Controller
      */
     public function home()
     {
-        return view('admin.dashboard.index');
+
+        $postCount = Listing::count(); // Get the total number of posts/listings
+        $likeCount = ListingLike::count(); // Get the total number of likes
+        $favoriteCount = Favorite::count(); // Get the total number of favorites
+        
+        return view('admin.dashboard.index', compact('postCount', 'likeCount', 'favoriteCount'));
     }
 }
